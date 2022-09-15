b99c5e3d3d3ab4901ffdbe0e552f2dd84cb81ee3
Eduardo Zielke <zielkeeduardo@gmail.com>
2022-09-15 19:29:24 -0300

    Removing babel

--- src/assets/Components/Counter/Counter.spec.jsx
+++ src/assets/Components/Counter/Counter.spec.jsx
@@ -1,13 +1,27 @@
-import { render } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+import { render, waitFor } from "@testing-library/react";
 import Counter from "./Counter";
-import * as React from "react";
 
 describe(Counter, () => {
-    it('counter displays correct initial value', () => {
+    it('counter displays correct initial value', async () => {
 
-        const { getByTestId } = render(<Counter initialValue={5}/>)
+        const { getByTestId, getByText } = render(<Counter initialValue={5}/>)
         const countValue = getByTestId('count').textContent
+        const incrementButton = getByText('Increment')
+        const decrementButton = getByText('Decrement')
 
         expect(countValue).toEqual("5")
+
+        userEvent.click(incrementButton);
+        
+        await waitFor(()=> {
+            expect(getByTestId('count').textContent).toEqual("6")
+        })
+
+        userEvent.click(decrementButton);
+
+        await waitFor(()=> {
+            expect(getByTestId('count').textContent).toEqual("5")
+        })
     })
 })
